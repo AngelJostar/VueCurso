4661582fa126ef19639779a4406ca6dcb9c5c2bf
AngelJostar <angelrojas@ciencias.unam.mx>
2022-12-08 16:53:26 -0600

    segundo commit

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,16 +1,71 @@
 <template>
-    <h2>Contador</h2>
-    <p>10</p>
+  <h2>{{customTitle}}</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+
+  <div>
+    <button v-on:click="increaseCounter()">+1</button>
+    <button @click="decreaseCounter()">-1</button>
+  </div>
 </template>
 
 <script>
+import { computed } from "@vue/runtime-core";
 export default {
+  props: {
+    title: String,
+    start: {
+        type: Number,
+        default: 10,
+        // required: true
+        validator(value){
+          return value >= 0
+        }
+    },
+  },
+  data() {
+    return {
+      counter: this.start,
+    };
+  },
+  methods: {
+    getSquareValue() {
+      return counter;
+    },
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    },
+  },
+  computed: {
+    squareCounter() {
+      console.log("");
+      return this.counter * this.counter;
+    },
+    customTitle(){
+        return this.title || 'Counter'
 
-}
+        // Primera forma
+        // return this.title ? this.title : 'Counter'
 
+        // Segunda Forma
+        // if(this.title){
+        //     return this.title
+        // }else{
+        //     return "Counter"
+        // }
+    }
+  },
+};
 </script>
 
 
 <style>
-
+button {
+  background-color: brown;
+  border-radius: 5px;
+  color: white;
+  margin: 0 5px;
+}
 </style>
